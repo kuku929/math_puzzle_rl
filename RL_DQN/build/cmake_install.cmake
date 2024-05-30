8d76efd6a4213a1b6ead51efec57218a62ea360b
kuku929 <krutarthpatel929@gmail.com>
2024-05-30 13:36:01 +0530

    removing useless binaries

--- RL_DQN/build/cmake_install.cmake
+++ RL_DQN/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/krutarth/Desktop/code/math_dc_project/math_puzzle_rl/RL_DQN
+# Install script for directory: /home/krutarth/Desktop/projects/code/math_dc_project/math_puzzle_rl/RL_DQN
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/krutarth/Desktop/code/math_dc_project/math_puzzle_rl/RL_DQN/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/krutarth/Desktop/projects/code/math_dc_project/math_puzzle_rl/RL_DQN/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
